a20c1d2126edd0049ec9c601e4d5a7510e778a6a
ProgNeo <prograneo@gmail.com>
2023-01-30 13:45:37 +0800

    Added ability to update slash commands.

--- delete-command.js
+++ delete-command.js
@@ -3,10 +3,11 @@ const { clientId, guildId, token } = require('./config.json');
 
 const rest = new REST({ version: '10' }).setToken(token);
 
-rest.delete(Routes.applicationGuildCommand(clientId, guildId, 'commandId'))
-	.then(() => console.log('Successfully deleted guild command'))
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: [] })
+	.then(() => console.log('Successfully deleted all guild commands.'))
 	.catch(console.error);
 
-rest.delete(Routes.applicationCommand(clientId, 'commandId'))
-	.then(() => console.log('Successfully deleted application command'))
+// for global commands
+rest.put(Routes.applicationCommands(clientId), { body: [] })
+	.then(() => console.log('Successfully deleted all application commands.'))
 	.catch(console.error);
