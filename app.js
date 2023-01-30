2a14b883218e3675288069bae16ef78de8a4173f
ProgNeo <prograneo@gmail.com>
2023-01-30 13:48:55 +0800

    Now the commands are processed correctly.

--- app.js
+++ app.js
@@ -15,18 +15,23 @@ const client = new Client({
 
 client.commands = new Collection();
 
-const commandsPath = path.join(__dirname, 'commands');
-const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandsDirectories = fs.readdirSync('./commands');
+const commandsFiles = [];
+for (const directory of commandsDirectories) {
+	const files = fs.readdirSync(`./commands/${directory}`).filter(file => file.endsWith('.js'));
+	for (const file of files) {
+		commandsFiles.push(`./commands/${directory}/${file}`);
+	}
+}
 
 for (const file of commandsFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
+	const command = require(file);
 
 	if ('data' in command && 'execute' in command) {
 		client.commands.set(command.data.name, command);
 	}
 	else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		console.log(`[WARNING] The command at ${file} is missing a required "data" or "execute" property.`);
 	}
 }
 
