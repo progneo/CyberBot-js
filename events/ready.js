4a86255fd4a85d2227b26e667fe92a9102a8426a
ProgNeo <prograneo@gmail.com>
2023-01-30 13:47:12 +0800

    Do not distribute mode

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,8 @@ module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
+		console.log(`Ready! Logged in as ${client.user.tag}\nID: ${client.user.id}`);
+		client.user.setStatus('dnd');
 		client.user.setActivity('dev', { type: 5 });
 	},
 };
