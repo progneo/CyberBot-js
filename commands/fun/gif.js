f0c9198f70efc40b9a353b12b5515230701d75a5
ProgNeo <prograneo@gmail.com>
2023-02-10 17:22:34 +0800

    Added 'gif' command

--- commands/fun/gif.js
+++ commands/fun/gif.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const axios = require('axios');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -9,11 +10,35 @@ module.exports = {
 				.setDescription('The gif category')
 				.setRequired(true)
 				.addChoices(
-					{ name: 'Funny', value: 'gif_funny' },
-					{ name: 'Meme', value: 'gif_meme' },
-					{ name: 'Movie', value: 'gif_movie' },
+					{ name: 'Air kiss', value: 'airkiss' },
+					{ name: 'Celebrate', value: 'celebrate' },
+					{ name: 'Clap', value: 'clap' },
+					{ name: 'Confused', value: 'confused' },
+					{ name: 'Cry', value: 'cry' },
+					{ name: 'Evil laugh', value: 'evillaugh' },
+					{ name: 'Hug', value: 'hug' },
+					{ name: 'Kiss', value: 'kiss' },
+					{ name: 'Laugh', value: 'laugh' },
+					{ name: 'Love', value: 'love' },
+					{ name: 'Nervous', value: 'nervous' },
+					{ name: 'No', value: 'no' },
+					{ name: 'Pat', value: 'pat' },
+					{ name: 'Peek', value: 'peek' },
+					{ name: 'Sad', value: 'sad' },
+					{ name: 'Scared', value: 'scared' },
+					{ name: 'Shy', value: 'shy' },
+					{ name: 'Slow clap', value: 'slowclap' },
+					{ name: 'Smile', value: 'smile' },
+					{ name: 'Sorry', value: 'sorry' },
+					{ name: 'Surprised', value: 'surprised' },
+					{ name: 'Tired', value: 'tired' },
+					{ name: 'Wink', value: 'wink' },
+					{ name: 'Yay', value: 'yay' },
+					{ name: 'Yes', value: 'yes' },
 				)),
 	async execute(interaction) {
 		const category = interaction.options.getString('category');
+		const result = await axios.get(`https://api.otakugifs.xyz/gif?reaction=kiss&format=${category}`);
+		await interaction.reply({ content: result.data['url'], ephemeral: false });
 	},
 };
